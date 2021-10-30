6f5860c24373a1ab0b53b942a03d1cde2318d415
Joakim G <jocke@gmail.com>
2021-10-30 19:25:42 +0200

    added actual period size to audi_info

--- audio.c
+++ audio.c
@@ -62,8 +62,10 @@ int audio_new(char *pcm_name,
     snd_pcm_hw_params_free(hw_params);
     return err;
   }
-  printf("Desired period size was %ld bytes and it was set to %ld\n",
-         desired_period_size_in_frames, period_size_in_frames);
+  if (desired_period_size_in_frames != period_size_in_frames) {
+    printf("NOTE: Desired period size was %ld bytes but it was set to %ld\n",
+           desired_period_size_in_frames, period_size_in_frames);
+  }
   
   snd_pcm_uframes_t desired_buffer_size_in_frames =
     period_size_in_frames * buffer_multiplicator;
@@ -74,8 +76,10 @@ int audio_new(char *pcm_name,
     snd_pcm_hw_params_free(hw_params);
     return err;
   }
-  printf("Desired buffer size was %ld bytes and it was set to %ld\n",
-         desired_buffer_size_in_frames, buffer_size_in_frames);
+  if (desired_buffer_size_in_frames != buffer_size_in_frames) {
+    printf("NOTE: Desired buffer size was %ld bytes but it was set to %ld\n",
+           desired_buffer_size_in_frames, buffer_size_in_frames);
+  }
 
   if ((err = snd_pcm_hw_params(pcm, hw_params)) < 0) {
     snd_pcm_hw_params_free(hw_params);
