e012390555824d9ba9396a3b5cb08d4d8e97f34d
Joakim G <jocke@gmail.com>
2021-11-18 00:52:55 +0100

    cosmetics

--- test/audio/purge_filenames_in_cwd.sh
+++ test/audio/purge_filenames_in_cwd.sh
@@ -1,5 +1,9 @@
-for oldname in *
-do
-  newname=`echo $oldname | sed -e 's/[ ()]//g'`
-  mv "$oldname" "$newname"
+#/bin/bash
+unzip -jo *.zip
+for f in *.wav; do
+    f2=`echo $f | sed -e 's/[ ()]//g'`
+    mv "${f}" "${f2}"
+    f3=${f2%.wav}.s16
+    echo "Converts ${f2} to ${f3}"
+    sox "${f2}" -c 2 -r 44100 -b 16 -e signed-integer --endian little -t raw "${f3}"
 done
