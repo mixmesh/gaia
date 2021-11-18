a8800e55c90c123e23afdb6e6327b7c5a6f93d30
Joakim G <jocke@gmail.com>
2021-11-18 01:01:49 +0100

    cosmetics

--- test/audio/munge_zip_files.sh
+++ test/audio/munge_zip_files.sh
@@ -2,7 +2,7 @@
 unzip -jo *.zip
 for f in *.wav; do
     f2=`echo $f | sed -e 's/[ ()]//g'`
-    mv "${f}" "${f2}"
+    mv "${f}" "${f2}" 2>/dev/null
     f3=${f2%.wav}.s16
     echo "Converts ${f2} to ${f3}"
     sox "${f2}" -c 2 -r 44100 -b 16 -e signed-integer --endian little -t raw "${f3}"
