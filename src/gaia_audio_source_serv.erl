f169959fdc6f61f44e09d1446ec45c031c299eed
Joakim G <jocke@gmail.com>
2021-12-07 22:16:58 +0100

    use alsa:strerror/1

--- src/gaia_audio_source_serv.erl
+++ src/gaia_audio_source_serv.erl
@@ -136,7 +136,7 @@ audio_producer_init(Params) ->
                         actual_sw_params => ActualSwParams}),
             audio_producer(AlsaHandle, PeriodSizeInFrames, []);
         {error, Reason} ->
-            exit(alsa:sterror(Reason))
+            exit(alsa:strerror(Reason))
     end.
 
 audio_producer(AlsaHandle, PeriodSizeInFrames, []) ->
@@ -180,5 +180,5 @@ audio_producer(AlsaHandle, PeriodSizeInFrames, CurrentSubscribers) ->
             audio_producer(AlsaHandle, PeriodSizeInFrames, Subscribers);
         {error, Reason} ->
             alsa:close(AlsaHandle),
-            exit(alsa:sterror(Reason))
+            exit(alsa:strerror(Reason))
     end.
