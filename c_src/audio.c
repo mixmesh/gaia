4933a5e4a301d6283f6adfce89fa5135cba8e0be
Joakim G <jocke@gmail.com>
2021-12-10 22:48:21 +0100

    added poll in non-blocking write

--- c_src/audio.c
+++ c_src/audio.c
@@ -174,16 +174,34 @@ int audio_read(audio_info_t *audio_info, uint8_t *data,
 
 int audio_non_blocking_write(audio_info_t *audio_info, uint8_t *data,
                              snd_pcm_uframes_t nframes) {
+    int count;
+    if ((count = snd_pcm_poll_descriptors_count(audio_info->pcm)) < 0) {
+        DEBUGF("snd_pcm_poll_descriptors_count: %d", count);
+        return count;
+    }
+    struct pollfd fds[count];
+    int err;
+    if ((err = snd_pcm_poll_descriptors(audio_info->pcm, fds, count)) < 0) {
+        DEBUGF("snd_pcm_poll_descriptors: %d", err);
+        return err;
+    }
+    unsigned short revents;
     ssize_t frame_size_in_bytes = snd_pcm_frames_to_bytes(audio_info->pcm, 1);
     snd_pcm_uframes_t written_frames = 0;
     while (written_frames < nframes) {
+        do {
+            poll(fds, count, -1);
+            snd_pcm_poll_descriptors_revents(audio_info->pcm, fds, count, &revents);
+            if (revents & POLLERR) {
+                return -EIO;
+            }
+        } while (!(revents & POLLOUT));
         snd_pcm_uframes_t frames =
             snd_pcm_writei(audio_info->pcm,
                            &data[written_frames * frame_size_in_bytes],
                            nframes - written_frames);
-        if (frames == -EAGAIN) {
-            return -EAGAIN;
-        } else if (frames < 0) {
+
+        if (frames < 0) {
             DEBUGF("snd_pcm_readi: Failed to write to audio device: %s",
                    snd_strerror(frames));
             int err;
