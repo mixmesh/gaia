bbf940f360390f2719e2807699e969fbd4e13e4e
Joakim G <jocke@gmail.com>
2021-12-11 11:15:21 +0100

    ...

--- src/test42.erl
+++ src/test42.erl
@@ -1,8 +1,10 @@
 -module(test42).
 -compile(export_all).
 
--define(CAPTURE_PCM_NAME, "plughw:0,0").
--define(PLAYBACK_PCM_NAME, "plughw:0,0").
+-define(CAPTURE_PCM_NAME, "bluealsa:DEV=20:74:CF:C5:28:79,PROFILE=sco").
+-define(PLAYBACK_PCM_NAME, "bluealsa:DEV=20:74:CF:C5:28:79,PROFILE=a2dp").
+%%-define(CAPTURE_PCM_NAME, "plughw:1,0").
+%%-define(PLAYBACK_PCM_NAME, "plughw:1,0").
 -define(FORMAT, s16_le).
 -define(CHANNELS, 1).
 -define(SAMPLE_SIZE_IN_BYTES, 2).
